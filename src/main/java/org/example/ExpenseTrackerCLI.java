c209374c2306e6a1b332e5cfe20d9d8fb620a93d
Mohit kumar <mohitk02082000@gmail.com>
2025-08-05 21:01:31 +0530

    initial commit

---
+++ src/main/java/org/example/ExpenseTrackerCLI.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class ExpenseTrackerCLI {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Expense Tracker CLI application.");
+    }
+}
