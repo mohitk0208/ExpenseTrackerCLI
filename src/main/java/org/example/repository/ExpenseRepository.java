47b4938662e24a91ad9b3af630bc77ea2efab22a
Mohit kumar <mohitk02082000@gmail.com>
2025-08-07 17:05:54 +0530

    created JsonExpenseRepository to handle CRUD operation with the JSON file

--- src/main/java/org/example/repository/ExpenseRepository.java
+++ src/main/java/org/example/repository/ExpenseRepository.java
@@ -3,13 +3,14 @@ package org.example.repository;
 import org.example.models.Expense;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ExpenseRepository {
 
     Expense save(Expense expense);
-    Expense getExpenseById(String id);
+    Optional<Expense> getExpenseById(String id);
     List<Expense> getAllExpenses();
     List<Expense> getAllExpensesByCategory(String category);
-    Void deleteExpense(String id);
+    void deleteExpense(String id);
     List<String> getAllExpenseCategory();
 }
---
+++ src/main/java/org/example/repository/JsonExpenseRepository.java
@@ -0,0 +1,111 @@
+package org.example.repository;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.example.models.Expense;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class JsonExpenseRepository implements ExpenseRepository {
+
+    private final ObjectMapper mapper;
+    private final File databaseFile;
+
+    public JsonExpenseRepository(String filePath) {
+        this.databaseFile = new File(filePath);
+        this.mapper = new ObjectMapper();
+
+        this.mapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+        try {
+            if (!databaseFile.exists()) {
+                boolean _ = databaseFile.getParentFile().mkdirs();
+                boolean __ = databaseFile.createNewFile();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(String.format("Could not create the Database file, filePath: (%s)%n", filePath));
+        }
+    }
+
+    @Override
+    public Expense save(Expense expense) {
+        if (expense.getId() != null && !expense.getId().isBlank()) {
+            writeExpensesToJsonFile(getAllExpenses().stream()
+                    .map(expense1 -> {
+                        if (expense1.getId().equals(expense.getId())) {
+                            return expense;
+                        }
+                        return expense;
+                    }).toList());
+        } else {
+            String id = UUID.randomUUID().toString();
+            expense.setId(id);
+            List<Expense> updatedExpenses = new ArrayList<>(getAllExpenses());
+            updatedExpenses.add(expense);
+            writeExpensesToJsonFile(updatedExpenses);
+        }
+
+        return expense;
+    }
+
+    @Override
+    public Optional<Expense> getExpenseById(String id) {
+        return getAllExpenses().stream()
+                .filter(expense -> expense.getId().equals(id))
+                .findFirst();
+    }
+
+    @Override
+    public List<Expense> getAllExpenses() {
+        try {
+            return this.mapper.readValue(this.databaseFile, new TypeReference<>() {
+            });
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to fetch expenses.");
+        }
+    }
+
+    @Override
+    public List<Expense> getAllExpensesByCategory(String category) {
+        return getAllExpenses().stream()
+                .filter(expense -> expense.getCategory().equalsIgnoreCase(category))
+                .toList();
+    }
+
+    @Override
+    public void deleteExpense(String id) {
+        Optional<Expense> expenseOptional = getExpenseById(id);
+
+        if (expenseOptional.isPresent()) {
+            writeExpensesToJsonFile(
+                    getAllExpenses().stream()
+                            .filter(expense -> !expense.getId().equals(id))
+                            .toList()
+            );
+        } else {
+            throw new IllegalArgumentException(String.format("Could not find Expense with id: (%s)%n", id));
+        }
+    }
+
+    @Override
+    public List<String> getAllExpenseCategory() {
+        return getAllExpenses().stream()
+                .map(Expense::getCategory)
+                .distinct()
+                .toList();
+    }
+
+    private void writeExpensesToJsonFile(List<Expense> expenses) {
+        try {
+            this.mapper.writeValue(this.databaseFile, expenses);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
